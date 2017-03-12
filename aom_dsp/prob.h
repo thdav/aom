53e9c12c06699bd49c2d5716d4bbdba990385990
Thomas Davies <thdavies@cisco.com>
2017-03-12 08:37:44 +0000

    Adjust rate.

--- aom_dsp/prob.h
+++ aom_dsp/prob.h
@@ -147,7 +147,7 @@ DECLARE_ALIGNED(16, extern const uint8_t, aom_norm[256]);
 
 #if CONFIG_EC_ADAPT
 static INLINE void update_cdf(aom_cdf_prob *cdf, int val, int nsymbs) {
-  const int rate = 4 + (cdf[nsymbs] > 31) + get_msb(nsymbs);
+  const int rate = 3 + (cdf[nsymbs] > 15) + (cdf[nsymbs] > 31) + get_msb(nsymbs);
   const int rate2 = 12 - rate;
   int i, tmp;
   int diff;
